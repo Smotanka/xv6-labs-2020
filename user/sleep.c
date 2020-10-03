7c4c4daf900f51325e5ce0758f8fb5b5dfe5778b
logeable <logeable@gmail.com>
2020-10-03 08:16:41 +0800

    lab1 sleep

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(2, "need more args\n");
+    }
+    int n = atoi(argv[1]);
+    if (sleep(n) != 0)
+    {
+        fprintf(2, "sleep failed\n");
+    }
+    exit(0);
+}
