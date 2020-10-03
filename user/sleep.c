13a38b3d601f223a7685869f66372deb2bb1aec8
logeable <logeable@gmail.com>
2020-10-03 08:25:44 +0800

    error check

--- user/sleep.c
+++ user/sleep.c
@@ -6,11 +6,13 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         fprintf(2, "need more args\n");
+        exit(1);
     }
     int n = atoi(argv[1]);
     if (sleep(n) != 0)
     {
         fprintf(2, "sleep failed\n");
+        exit(1);
     }
     exit(0);
 }
